06765b0f04e5c23321cbc9877b40eec617ca6f5e
xgmxgm <ekalkabekov@gmail.com>
2024-07-20 15:37:39 +0500

    init

--- src/shared/libs/axios/init/axios.ts
+++ src/shared/libs/axios/init/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-	baseURL: 'http://localhost:4444',
+	baseURL: process.env.SERVER_URL || 'http://localhost:4444',
 })
 
 export default instance
