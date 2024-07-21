7a144fc03920cb77c4d09b47cf963126872b59b2
xgmxgm <ekalkabekov@gmail.com>
2024-07-21 17:51:57 +0500

    init

--- src/shared/libs/api/init/index.ts
+++ src/shared/libs/api/init/index.ts
@@ -4,7 +4,8 @@ export const rtkApi = createApi({
 	reducerPath: 'api',
 	tagTypes: ['Task'],
 	baseQuery: fetchBaseQuery({
-		baseUrl: process.env.SERVER_URL || 'http://localhost:4444',
+		// baseUrl: process.env.SERVER_URL,
+		baseUrl: 'https://todobackend-production-77ec.up.railway.app',
 	}),
 	endpoints: () => ({}),
 })
--- src/shared/libs/axios/init/axios.ts
+++ src/shared/libs/axios/init/axios.ts
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const instance = axios.create({
-	baseURL: process.env.SERVER_URL,
+	// baseURL: process.env.SERVER_URL,
+	baseURL: 'https://todobackend-production-77ec.up.railway.app',
 })
 
 export default instance
